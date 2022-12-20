b757647f7a4062b7d3145a6858cedcf9cb7b721d
Marta <maardiakiv@gmail.com>
2022-12-20 18:56:18 +0100

    initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# goit-react-hw-02-feedback
+
+Homework 02
+
+[GitHub Pages](https://imartete.github.io/goit-react-hw-02-feedback/)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Section } from 'components/Section';
+export class App extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Section title=""></Section>
+      </div>
+    );
+  }
+}
